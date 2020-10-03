fac3575cec44e94e1577e3354a8840fc8b778b99
jackale <melon119@gmail.com>
2020-10-04 01:45:08 +0900

    Bye :wave:

--- js/change.js
+++
@@ -1,8 +0,0 @@
-function ChangeTab(tabname) {
-   document.getElementById('home').style.display = 'none';
-   document.getElementById('about').style.display = 'none';
-   document.getElementById('product').style.display = 'none';
-   document.getElementById('contact').style.display = 'none';
-   
-   document.getElementById(tabname).style.display = 'block';
-}
